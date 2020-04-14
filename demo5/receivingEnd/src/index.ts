09354f4d937052e69d01e73f06a01ffdab62fbd9
jongwong <277061456@qq.com>
2020-04-14 11:41:45 +0800

    demo5 init

---
+++ demo5/receivingEnd/src/index.ts
@@ -0,0 +1,22 @@
+// @ts-ignore
+import TRTC from "trtc-js-sdk";
+// @ts-ignore
+import TIM from 'tim-js-sdk';
+// @ts-ignore
+import COS from "cos-js-sdk-v5";
+/*
+const client = TRTC.createClient({
+  mode: "videoCall",
+  sdkAppId,
+  userId,
+  userSig,
+});
+*/
+
+const roomId = "1234567890";
+const sdkAppId = "1400345310";
+const userId = "1";
+const userSig =
+    "eJyrVgrxCdYrSy1SslIy0jNQ0gHzM1NS80oy0zLBwoZQweKU7MSCgswUJStDEwMDYxNTY0MDiExqRUFmUSpQ3NTU1MjAACpakpkLFrMwtbQ0MDE1gpqSmQ40MyU9zCgi3yvDLzkv3bwwpCLSoyzJsMQvK8jNxNfXPze1KNwsy6jY1cey0MRWqRYAFy8vzw__";
+
+
---
+++ demo5/sendEnd/src/index.ts
@@ -0,0 +1,23 @@
+// @ts-ignore
+import TRTC from "trtc-js-sdk";
+// @ts-ignore
+import TIM from 'tim-js-sdk';
+// @ts-ignore
+import COS from "cos-js-sdk-v5";
+
+const toUserId = "2";
+const sdkAppId = "1400345310";
+const userId = "1";
+const userSig =
+    "eJyrVgrxCdYrSy1SslIy0jNQ0gHzM1NS80oy0zLBwoZQweKU7MSCgswUJStDEwMDYxNTY0MDiExqRUFmUSpQ3NTU1MjAACpakpkLFrMwtbQ0MDE1gpqSmQ40MyU9zCgi3yvDLzkv3bwwpCLSoyzJsMQvK8jNxNfXPze1KNwsy6jY1cey0MRWqRYAFy8vzw__";
+
+
+/*
+const toUserId = "test1";
+const sdkAppId = "1400349760";
+const userId = "jongwong";
+const userSig =
+    "eJyrVgrxCdYrSy1SslIy0jNQ0gHzM1NS80oy0zLBwln5eenlQAyVK07JTiwoyExRsjI0MTAwNrE0NzOAyKRWFGQWpQLFTU1NjQwMoKIlmblgMQszc3NjUzNDqCmZ6SAbU7Utg-ICKx3NfD380zMC3TI90gLdIqr8snPNg8MMwixMS3LdItzzc-yTbZVqAfSdMoc_";
+*/
+
+
