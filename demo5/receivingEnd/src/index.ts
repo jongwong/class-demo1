8a5a7878ad9c047ea2f1d60898c5a82dd7c6becc
jongwong <277061456@qq.com>
2020-04-14 11:32:08 +0800

    demo5 init

---
+++ demo5/receivingEnd/src/index.ts
@@ -0,0 +1,27 @@
+// @ts-ignore
+import TRTC from "trtc-js-sdk";
+// @ts-ignore
+import TIM from 'tim-js-sdk';
+// @ts-ignore
+import COS from "cos-js-sdk-v5";
+/*
+const client = TRTC.createClient({
+  mode: "videoCall",
+  sdkAppId,
+  userId,
+  userSig,
+});
+*/
+
+const sdkAppId = "1400345310";
+const userId = "2";
+const userSig = "eJwtzMkKwjAUheF3yVrqbYZO4FLikC6qdqG4ERPjNVhCDSKI725pszzfgf9LDmqfvE1PKkITILNxozZdwBtOHPGl3cV71KRKOQDjgqUwPebjsTeDCyEoQNSAz9EKUZYgWBEraIcmnLJd3WCwOVe521qWSaeX67aRUtXt-XGe080RVHfFFSzI7w8bKS7D"
+
+/*
+const sdkAppId = "1400349760";
+const userId = "test1";
+const userSig ="eJwtzMsKwjAQheF3yVrqpG2SptCFKAWxtIu6EdyImcpUlJAG8YLvbolZnu-A-2H7pk8e6FjJ0gTYImwyePc0UGCPk*fxmMz1ZC0ZVvIcIMu1kvB-8GnJ4exCiBQgqqdbsEIqlUmexwpd5m5Xa5QjHJfFiI0RPnNt4aSyu9eBtvW7lVbjetWZfjOcK-b9ATm2MRw_"
+
+*/
+
+
